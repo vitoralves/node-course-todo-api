0f7615bed31f7296111b1da36602ef0313b7a36c
vitor <v.alves@eddydata.com>
2019-02-05 11:51:07 -0200

    get todo by id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,32 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {ObjectID} = require('mongodb');
+const {User} = require('./../server/models/user');
+
+// var id = '5c59779b9fee903ee45d93ed';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log(todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log(todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     console.log(todo);
+// });
+
+User.find({
+    email: 'test@test.com'
+}).then((user) => {
+    console.log(user);
+});
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+var { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -24,8 +23,24 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos });
-    }, (e) => res.status(400).send(e));
+    }).catch((e) => {
+        res.status(400).send(e)
+    });
 });
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (ObjectID.isValid(id)) {
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({ todo });
+        }, (e) => res.status(400).send(e));
+    } else {
+        res.status(400).send({message: 'Invalid ID number'});
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
