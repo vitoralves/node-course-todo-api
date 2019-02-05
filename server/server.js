cf6be28ed62ee1dde20a7aad8bb6d68da4a02a11
vitor <v.alves@eddydata.com>
2019-02-05 09:46:59 -0200

    tests to get all todos

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => res.status(400).send(e));
 });
 
@@ -31,4 +31,4 @@ app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
