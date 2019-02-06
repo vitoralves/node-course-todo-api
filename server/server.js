3f9ac18e07661b7abf110c39be9e980dfb519811
Vitor Diogo Alves <vitor.diogo.live@hotmail.com>
2019-02-05 23:19:55 -0200

    method to update a todo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {mongoose} = require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
 var { Todo } = require('./models/todo');
-var { ObjectID } = require('mongodb');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -56,6 +58,30 @@ app.delete('/todos/:id', (req, res) => {
     } else {
         res.status(400).send({message: 'Invalid ID number'});
     }
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return send.status(400, send({ message: 'Invalid ID' }));
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(400).send({message: 'Todo not found'});
+        }
+
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
