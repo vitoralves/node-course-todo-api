806890e05c2bd230741c45da5b3cde16b109663a
vitor <v.alves@eddydata.com>
2019-02-15 17:14:34 -0200

    inicio de rota de login

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 const { mongoose } = require('./db/mongoose');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -100,6 +101,25 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    User.findOne({ 'email': req.body.email }).then((user) => {
+        if (!user) {
+            res.status(400).send('Wrong email!');
+        }
+        bcrypt.compare(req.body.password, user.password, (err, compare) => {
+            if (err) {
+                res.status(400).send();
+            }
+
+            if (compare) {
+                res.status(200).send(user);
+            } else {
+                res.status(400).send('Wrong password!');
+            }
+        })
+    }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => {
