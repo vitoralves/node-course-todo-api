22f9ce6c3fdd4e58bb4deb9c62b2183e5f842474
Vitor Diogo Alves <vitor.diogo.live@hotmail.com>
2019-03-02 12:35:19 -0300

    improve config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTEST';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
